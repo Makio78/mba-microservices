46b652f701d49661cfd9b23f54db53b979c678ed
Lincoln Silva Carvalho <lynixcarvalho@gmail.com>
2022-11-10 11:50:36 -0300

    Correção de erros

--- trilhas/src/main/java/br/com/fiap/mba/microservices/trilhas/TrilhasApplication.java
+++ trilhas/src/main/java/br/com/fiap/mba/microservices/trilhas/TrilhasApplication.java
@@ -2,11 +2,9 @@ package br.com.fiap.mba.microservices.trilhas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableMongoRepositories
 public class TrilhasApplication {
 
